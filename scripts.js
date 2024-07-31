bdd0e69087dc9d84d5c05855e479e6381312582a
Bryce <vicknairbp@gmail.com>
2024-07-30 20:16:43 -0700

    made a shopping cart

--- scripts.js
+++ scripts.js
@@ -1,23 +1,36 @@
 
 window.onload = function() {
-    let products = localStorage.getItem("cart");
-    if (products == null) {
-        products = [];
-    }
-    else {
-        products = JSON.parse(localStorage.getItem("cart"));
-    }
-    const buttons = document.querySelectorAll(".product button");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click",  () => {
-            let product = buttons[i].dataset.product;
-            products.push(product);
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((json) => {
+            let products = document.querySelector(".products");
+            json.forEach((product) => {
+                var newElement = '<a class="product">'+
+                    `<div><img src="${product.picture}"/></div>`+
+                    `<div>${product.name}</div>`+
+                    `<div class="price">$${product.price}</div>`+
+                    `<button data-product="${product.id}">Add to cart</button>`+
+                '</a>';
+                products.innerHTML += newElement;
+            });
+            let idsInCart = localStorage.getItem("cart");
+            if (idsInCart == null) {
+                idsInCart = [];
+            }
+            else {
+                idsInCart = JSON.parse(localStorage.getItem("cart"));
+            }
+            const buttons = document.querySelectorAll(".product button");
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].addEventListener("click",  () => {
+                    let product = buttons[i].dataset.product;
+                    idsInCart.push(product);
+                    let idcount = document.querySelector(".cart sup");
+                    idcount.innerText = idsInCart.length;
+                    localStorage.setItem("cart", JSON.stringify(idsInCart));
+                });
+            }
             let idcount = document.querySelector(".cart sup");
-            idcount.innerText = products.length;
-            localStorage.setItem("cart", JSON.stringify(products));
+            idcount.innerText = idsInCart.length;
         });
-    }
-    let idcount = document.querySelector(".cart sup");
-    idcount.innerText = products.length;
-    
 };
