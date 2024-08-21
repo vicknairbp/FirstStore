77dfde69a7c64181e5eb4c54b9e46e93cb1c0367
Bryce <vicknairbp@gmail.com>
2024-08-20 17:23:38 -0700

    making order return work

--- client/cartpage.js
+++ client/cartpage.js
@@ -130,7 +130,6 @@ window.onload = function() {
     },
   })
   .render("#paypal-button-container");
-
   // Example function to show a result to the user. Your site's UI library can be used instead.
   function resultMessage(message) {
     const container = document.querySelector("#result-message");
